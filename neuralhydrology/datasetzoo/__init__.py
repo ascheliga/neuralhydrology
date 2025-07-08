a32363363ecbaaa36f0eba54b7a11bda57b2f017
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-07-08 15:28:32 -0700

    created custom dataset type

--- neuralhydrology/datasetzoo/__init__.py
+++ neuralhydrology/datasetzoo/__init__.py
@@ -7,6 +7,7 @@ from neuralhydrology.datasetzoo.camelsus import CamelsUS
 from neuralhydrology.datasetzoo.caravan import Caravan
 from neuralhydrology.datasetzoo.genericdataset import GenericDataset
 from neuralhydrology.datasetzoo.hourlycamelsus import HourlyCamelsUS
+from neuralhydrology.datasetzoo.cygnssres import CYGNSSres
 from neuralhydrology.datasetzoo.lamah import LamaH
 from neuralhydrology.utils.config import Config
 
@@ -77,6 +78,8 @@ def get_dataset(cfg: Config,
         Dataset = LamaH
     elif cfg.dataset.lower() == "caravan":
         Dataset = Caravan
+    elif cfg.dataset.lower() == "cygnssres":
+        Dataset = CYGNSSres
     else:
         raise NotImplementedError(f"No dataset class implemented for dataset {cfg.dataset}")
 
