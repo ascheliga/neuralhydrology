0abff17e65faddfb889f5dcbafd6f5e2d71bdf3b
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-07-08 15:31:19 -0700

    formatting cygnss as an input

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=run_nh_test_attempt5
+#SBATCH --job-name=run_nh_cygnss_test_attempt0
 #SBATCH --account=fc_ecohydrology
 #SBATCH --partition=savio2_htc
-#SBATCH --time=00:05:15
+#SBATCH --time=00:01:15
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=3
 
 ## Command(s) to run:
 
 eval "$(conda shell.bash hook)"
 
 conda activate neuralhydrology
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/examples/01-Introduction/1_basin.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/adding_data/custom_with_CYGNSS.yml
