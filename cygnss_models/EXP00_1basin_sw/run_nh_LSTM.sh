5d845e8b2b2495a64444e354aa720ee8a7f713f3
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-08-07 16:06:49 -0700

    re ran MSE loss

--- cygnss_models/EXP00_1basin_sw/run_nh_LSTM.sh
+++ cygnss_models/EXP00_1basin_sw/run_nh_LSTM.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=run_nh_LSTM_EXP00_niger_and_cbr_att02
+#SBATCH --job-name=run_nh_LSTM_EXP00_MSE_att00
 #SBATCH --account=fc_ecohydrology
 #SBATCH --partition=savio2_htc
 #SBATCH --time=00:10:15
@@ -9,10 +9,10 @@
 eval "$(conda shell.bash hook)"
 conda activate neuralhydrology
 
-# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/kainji_wi_sw.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/kainji_wi_sw_MSE.yml
 
-# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/kainji_no_sw.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/kainji_no_sw_MSE.yml
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/powell_wi_sw.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/powell_wi_sw_MSE.yml
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/powell_no_sw.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/powell_no_sw_MSE.yml
