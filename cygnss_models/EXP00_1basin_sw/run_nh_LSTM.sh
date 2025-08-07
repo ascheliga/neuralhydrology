32d3530b35987bdcdf8140783ac1e8e59ef5a488
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-08-07 14:32:51 -0700

    creating yml file for each run

--- cygnss_models/EXP00_1basin_sw/run_nh_LSTM.sh
+++ cygnss_models/EXP00_1basin_sw/run_nh_LSTM.sh
@@ -7,14 +7,6 @@
 #SBATCH --ntasks-per-node=2
 
 
-
-eval "$(conda shell.bash hook)"
-conda activate rioxarray_env
-
-# pre-processing
-python LSTM_preprocessing.py
-
-
 conda activate neuralhydrology
 
 nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/1basin_wi_sw.yml
