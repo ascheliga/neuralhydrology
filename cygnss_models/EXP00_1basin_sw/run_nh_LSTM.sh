3f49382b23a713c3f5a1160d892120dba437b0c1
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-08-07 14:46:01 -0700

    renaming runs

--- cygnss_models/EXP00_1basin_sw/run_nh_LSTM.sh
+++ cygnss_models/EXP00_1basin_sw/run_nh_LSTM.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=run_nh_LSTM_EXP00_niger_att02
+#SBATCH --job-name=run_nh_LSTM_EXP00_niger_and_cbr_att00
 #SBATCH --account=fc_ecohydrology
 #SBATCH --partition=savio2_htc
-#SBATCH --time=00:05:15
+#SBATCH --time=00:10:15
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 
 
 conda activate neuralhydrology
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/1basin_wi_sw.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/kainji_wi_sw.yml
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/1basin_no_sw.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/kainji_no_sw.yml
+
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/powell_wi_sw.yml
+
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/powell_no_sw.yml
