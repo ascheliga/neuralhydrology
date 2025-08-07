26582bab3c302a04a8c3083d1f884dc14d79e53a
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-08-07 14:50:57 -0700

    fixed filepath typos

--- cygnss_models/EXP00_1basin_sw/run_nh_LSTM.sh
+++ cygnss_models/EXP00_1basin_sw/run_nh_LSTM.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=run_nh_LSTM_EXP00_niger_and_cbr_att00
+#SBATCH --job-name=run_nh_LSTM_EXP00_niger_and_cbr_att02
 #SBATCH --account=fc_ecohydrology
 #SBATCH --partition=savio2_htc
 #SBATCH --time=00:10:15
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 
-
+eval "$(conda shell.bash hook)"
 conda activate neuralhydrology
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/kainji_wi_sw.yml
+# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/kainji_wi_sw.yml
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/kainji_no_sw.yml
+# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/kainji_no_sw.yml
 
 nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP00_1basin_sw/powell_wi_sw.yml
 
