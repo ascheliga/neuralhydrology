02d45480720542934b1c0b49cef9e6184124f8e1
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-08-12 12:23:26 -0700

    added 10day seq

--- cygnss_models/EXP03_seqlen/run_nh_LSTM.sh
+++ cygnss_models/EXP03_seqlen/run_nh_LSTM.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=run_nh_LSTM_EXP03_306090_att00
+#SBATCH --job-name=run_nh_LSTM_EXP03_10_att00
 #SBATCH --account=fc_ecohydrology
 #SBATCH --partition=savio2_htc
-#SBATCH --time=00:03:15
+#SBATCH --time=00:01:15
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 
 eval "$(conda shell.bash hook)"
 conda activate neuralhydrology
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_wi_sw_30.yml
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_wi_sw_60.yml
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_wi_sw_90.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_wi_sw_10.yml
+# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_wi_sw_30.yml
+# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_wi_sw_60.yml
+# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_wi_sw_90.yml
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_no_sw_30.yml
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_no_sw_60.yml
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_no_sw_90.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_no_sw_10.yml
+# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_no_sw_30.yml
+# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_no_sw_60.yml
+# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_no_sw_90.yml
