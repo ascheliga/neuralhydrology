4627a7a979c94a1ad58c60d384f4391ca3847ad9
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-08-12 12:23:04 -0700

    reran exps

--- cygnss_models/EXP02_transfer/run_nh_LSTM.sh
+++ cygnss_models/EXP02_transfer/run_nh_LSTM.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=run_nh_LSTM_EXP02_1-2_2-1_att00
+#SBATCH --job-name=run_nh_LSTM_EXP02_12_21_att02
 #SBATCH --account=fc_ecohydrology
 #SBATCH --partition=savio2_htc
-#SBATCH --time=00:20:15
+#SBATCH --time=00:04:15
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 
 eval "$(conda shell.bash hook)"
 conda activate neuralhydrology
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/powell_no_sw_1-2.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/powell_no_sw_1_2.yml
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/powell_wi_sw_1-2.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/powell_wi_sw_1_2.yml
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/powell_no_sw_2-1.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/powell_no_sw_2_1.yml
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/powell_wi_sw_2-1.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/powell_wi_sw_2_1.yml
