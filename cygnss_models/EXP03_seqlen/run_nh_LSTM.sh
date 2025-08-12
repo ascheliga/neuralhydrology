5a18afc4d76349e10c9cb36d26520514372e1e0b
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-08-12 12:04:51 -0700

    created sequence length experiment folder

---
+++ cygnss_models/EXP03_seqlen/run_nh_LSTM.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=run_nh_LSTM_EXP03_306090_att00
+#SBATCH --account=fc_ecohydrology
+#SBATCH --partition=savio2_htc
+#SBATCH --time=00:03:15
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+
+eval "$(conda shell.bash hook)"
+conda activate neuralhydrology
+
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_wi_sw_30.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_wi_sw_60.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_wi_sw_90.yml
+
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_no_sw_30.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_no_sw_60.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP03_seqlen/powell_no_sw_90.yml
