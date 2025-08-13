491ce1e6f5b3f0e52400879a1597119aaf203690
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-08-12 18:17:35 -0700

    split into tot0 and ex met input subfolders

--- cygnss_models/EXP04_general/run_nh_LSTM.sh
+++ cygnss_models/EXP04_general/run_nh_LSTM.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=run_nh_LSTM_EXP04_att00
+#SBATCH --job-name=run_nh_LSTM_EXP04__exinputs_att00
 #SBATCH --account=fc_ecohydrology
 #SBATCH --partition=savio2_htc
-#SBATCH --time=00:01:15
+#SBATCH --time=00:02:35
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 
 eval "$(conda shell.bash hook)"
 conda activate neuralhydrology
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP04_general/general_no_sw.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP04_general/ex_inputs/general_no_sw.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP04_general/ex_inputs/general_wi_sw.yml
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP04_general/general_wi_sw.yml
+
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP04_general/tot0_inputs/general_no_sw.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP04_general/tot0_inputs/general_wi_sw.yml
