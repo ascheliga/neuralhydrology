b392cd8f1fc5db7a8310d691980e2cfc40a3d064
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-08-16 19:17:35 -0700

    adding new basins: ord, orange, magdalena, ne_aus

--- cygnss_models/preprocessing/run_preprocessing.sh
+++ cygnss_models/preprocessing/run_preprocessing.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=crb_extra_eda_att00
+#SBATCH --job-name=ord_extra_ext_met_att01
 #SBATCH --account=fc_ecohydrology
 #SBATCH --partition=savio2_htc
-#SBATCH --time=00:01:30
+#SBATCH --time=00:45:30
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 
@@ -11,10 +11,10 @@
 eval "$(conda shell.bash hook)"
 conda activate rioxarray_env
 
-export yml_file='colorado_full_basin.yml'
+export yml_file='ord_basin.yml'
 
 # pre-processing into DataFrame
-# python LSTM_preprocessing.py
+python LSTM_preprocessing.py
 
 # plotting info
 python eda_preprocessing.py
