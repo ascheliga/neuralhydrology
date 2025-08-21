3cf7ebb990a81da4cb28338b71fa665ebbb099de
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-08-21 15:54:08 -0700

    created EA-LSTM vs cudaLSTM exp

---
+++ cygnss_models/EXP06_modeltype/run_nh_LSTM.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=run_nh_LSTM_EXP06_modeltype_debug_att00
+#SBATCH --account=fc_ecohydrology
+#SBATCH --partition=savio2_htc
+#SBATCH --time=00:02:35
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+
+eval "$(conda shell.bash hook)"
+conda activate neuralhydrology
+
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP06_modeltype/ealstm_no_sw.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP06_modeltype/ealstm_wi_sw.yml
+
+
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP06_modeltype/cuda_no_sw.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP06_modeltype/cuda_wi_sw.yml
