4bef1247c9a04aad5d25a6c9243a01f0f2528ec0
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-08-21 11:27:51 -0700

    reorganized EXP and updated with basin attrs

--- cygnss_models/EXP05_hiddenstates/run_nh_LSTM.sh
+++ cygnss_models/EXP05_hiddenstates/run_nh_LSTM.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=run_nh_LSTM_EXP05_hidden_att00
+#SBATCH --job-name=run_nh_LSTM_EXP05_hiddenatt00
 #SBATCH --account=fc_ecohydrology
 #SBATCH --partition=savio2_htc
-#SBATCH --time=00:30:15
+#SBATCH --time=01:01:15
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 
@@ -10,8 +10,8 @@ eval "$(conda shell.bash hook)"
 conda activate neuralhydrology
 
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP05_hiddenstates/seqlen_270/general_wi_sw_20.yml
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP05_hiddenstates/seqlen_270/general_wi_sw_256.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP05_hiddenstates/general_wi_sw_20.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP05_hiddenstates/general_wi_sw_256.yml
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP05_hiddenstates/seqlen_270/general_no_sw_20.yml
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP05_hiddenstates/seqlen_270/general_no_sw_256.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP05_hiddenstates/general_no_sw_20.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP05_hiddenstates/general_no_sw_256.yml
