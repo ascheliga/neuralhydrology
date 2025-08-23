1a73ccb603f7174027d231fb4abf0095e29a3a0f
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-08-22 19:50:40 -0700

    updated with 10 basins

--- cygnss_models/EXP04_general/run_nh_LSTM.sh
+++ cygnss_models/EXP04_general/run_nh_LSTM.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=run_nh_LSTM_EXP04_basin_attr_att01
+#SBATCH --job-name=run_nh_LSTM_EXP04_10basin_att00
 #SBATCH --account=fc_ecohydrology
 #SBATCH --partition=savio2_htc
-#SBATCH --time=00:35:35
+#SBATCH --time=01:35:35
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 
@@ -13,5 +13,5 @@ conda activate neuralhydrology
 # nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP04_general/ex_inputs/general_wi_sw.yml
 
 
-# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP04_general/tot0_inputs/general_no_sw.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP04_general/tot0_inputs/general_no_sw.yml
 nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP04_general/tot0_inputs/general_wi_sw.yml
--- cygnss_models/EXP05_hiddenstates/run_nh_LSTM.sh
+++ cygnss_models/EXP05_hiddenstates/run_nh_LSTM.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=run_nh_LSTM_EXP05_hidden_att01
+#SBATCH --job-name=run_nh_LSTM_EXP05_hidden_10basin_att00
 #SBATCH --account=fc_ecohydrology
 #SBATCH --partition=savio2_htc
 #SBATCH --time=05:01:15
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=3
 
 eval "$(conda shell.bash hook)"
 conda activate neuralhydrology
 
 
-# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP05_hiddenstates/general_wi_sw_20.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP05_hiddenstates/general_wi_sw_20.yml
 nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP05_hiddenstates/general_wi_sw_256.yml
 
 nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP05_hiddenstates/general_no_sw_20.yml
