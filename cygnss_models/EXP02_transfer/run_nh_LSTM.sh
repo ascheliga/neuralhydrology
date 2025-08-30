abc1f22397b1fbd4956ad2a273058ae1bc754018
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-08-29 17:41:25 -0700

    rerunning 10-1, running 9-1

--- cygnss_models/EXP02_transfer/run_nh_LSTM.sh
+++ cygnss_models/EXP02_transfer/run_nh_LSTM.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-#SBATCH --job-name=run_nh_LSTM_EXP02_n8of8_att00
+#SBATCH --job-name=run_nh_LSTM_EXP02_n1of8_att01
 #SBATCH --account=fc_ecohydrology
-#SBATCH --partition=savio2_htc
-#SBATCH --time=02:04:15
+#SBATCH --partition=savio2
+#SBATCH --time=01:35:15
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
+## SBATCH --ntasks-per-node=2
 
 eval "$(conda shell.bash hook)"
 conda activate neuralhydrology
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/no_sw_1_2.yml
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/wi_sw_1_2.yml
+# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/no_sw_1_2.yml
+# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/wi_sw_1_2.yml
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/no_sw_2_1.yml
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/wi_sw_2_1.yml
+# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/no_sw_2_1.yml
+# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/wi_sw_2_1.yml
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/no_sw_9_2.yml
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/wi_sw_9_2.yml
+# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/no_sw_9_2.yml
+# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/wi_sw_9_2.yml
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/no_sw_10_1.yml
+# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/no_sw_10_1.yml
 nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/wi_sw_10_1.yml
+
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/no_sw_9_1.yml
+nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/wi_sw_9_1.yml
