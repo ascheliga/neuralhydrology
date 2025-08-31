37fa6f4ae6d625d4febdcb1e24c3c02e05cc9fd4
Ann Scheliga <ann_scheliga@berkeley.edu>
2025-08-30 23:43:59 -0700

    evaluated ensemble transfer learning

--- cygnss_models/EXP02_transfer/run_nh_LSTM.sh
+++ cygnss_models/EXP02_transfer/run_nh_LSTM.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=run_nh_LSTM_EXP02_n8of8_att01
+#SBATCH --job-name=run_nh_LSTM_EXP02_cleanup_att03
 #SBATCH --account=fc_ecohydrology
 #SBATCH --partition=savio2
-#SBATCH --time=01:05:15
+#SBATCH --time=00:25:15
 #SBATCH --nodes=1
 ## SBATCH --ntasks-per-node=2
 
@@ -19,7 +19,7 @@ conda activate neuralhydrology
 # nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/wi_sw_9_2.yml
 
 # nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/no_sw_10_1.yml
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/wi_sw_10_1.yml
+# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/wi_sw_10_1.yml
 
-nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/no_sw_9_1.yml
+# nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/no_sw_9_1.yml
 nh-run train --config-file /global/home/users/ann_scheliga/neuralhydrology/cygnss_models/EXP02_transfer/wi_sw_9_1.yml
